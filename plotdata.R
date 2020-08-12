e1b97dbd1fb07fa3fb9503011b952e0d1c692519
Naimin Jing <naimin.jing@merck.com>
2020-08-12 12:26:21 -0400

    Add plot title

--- plotdata.R
+++ plotdata.R
@@ -4,6 +4,7 @@ data <- data.frame(x=1:10, y=(1:10)*3)
 
 ggplot(data, aes(x = x, y = y))+
   geom_line(color='red')+
+  ggtitle("The Plot of y versus x")
   theme_bw()
 
 ggsave("example.png", width = 7, height = 5)
