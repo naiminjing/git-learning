f107006fcf8e61b7432cec2937db338b26f3c160
Naimin Jing <naimin.jing@merck.com>
2020-08-12 11:09:06 -0400

    Change the color of the line

--- plotdata.R
+++ plotdata.R
@@ -3,7 +3,7 @@ library(ggplot2)
 data <- data.frame(x=1:10, y=(1:10)*3)
 
 ggplot(data, aes(x = x, y = y))+
-  geom_line()+
+  geom_line(color='red')+
   theme_bw()
 
 ggsave("example.png", width = 7, height = 5)
